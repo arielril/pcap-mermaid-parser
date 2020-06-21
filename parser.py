ba79d93a4d71d8428dc0cefed54b0c54a900208d
Ariel Ril <ariel.ril@4all.com>
2020-06-21 12:00:14 -0300

    add argparse

--- parser.py
+++ parser.py
@@ -1,3 +1,5 @@
+import sys
+import argparse
 import socket
 import dpkt
 
@@ -6,11 +8,42 @@ from eth_packet.arp import parse as parseARP
 from ip_packet.ip import parseV4 as parseIPv4
 
 
-if __name__ == "__main__":
-    filepath = './examples/arp_pcap.pcap'
-    n_packet = 20
+def build_argparse() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        description='PCAP to Mermaid file parser',
+    )
 
-    f = open(filepath, 'rb')
+    parser.add_argument(
+        'file_path',
+        metavar='file_path',
+        type=str,
+        help='Path for the .pcap file to parse'
+    )
+    parser.add_argument(
+        '-c', '--count',
+        metavar='pkt_count',
+        type=int,
+        help='Number of packets to parse',
+        default=999999,
+        dest='pkt_count',
+    )
+
+    return parser
+
+
+if __name__ == "__main__":
+    parser = build_argparse()
+    args = parser.parse_args()
+
+    filepath = args.file_path
+    n_packet = args.pkt_count
+
+    try:
+        f = open(filepath, 'rb')
+    except FileNotFoundError:
+        print('Invalid file path')
+        sys.exit(1)
+
     pcap_f = dpkt.pcap.Reader(f)
 
     result = ['sequenceDiagram']
